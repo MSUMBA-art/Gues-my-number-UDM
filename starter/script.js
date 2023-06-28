61cc897a58eff0e5abba482e19e722653f4e8346
ALEXANDER <msumba83@gmail.com>
2023-06-28 23:04:36 +0200

    manupulated css and add click event to again button

--- starter/script.js
+++ starter/script.js
@@ -10,10 +10,10 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
+// click  event for check button
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
@@ -25,13 +25,13 @@ document.querySelector('.check').addEventListener('click', function () {
     // when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '💪 Correct Number';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.message').style.color = 'red';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').style.color = 'blue';
-    
 
     // when guess is too hight
   } else if (guess > secretNumber) {
@@ -57,3 +57,30 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// click event for again button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+   document.querySelector('.number').style.color = '#444';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+/*Coding Challenge #1
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK 😀
+*/
