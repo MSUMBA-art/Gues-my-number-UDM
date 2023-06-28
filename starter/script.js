7b8b9e46b5ac61082f4b9cdd6761c5606d17acb2
ALEXANDER <msumba83@gmail.com>
2023-06-28 06:13:16 +0200

    manipulated css

--- starter/script.js
+++ starter/script.js
@@ -10,11 +10,50 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number( document.querySelector('.guess').value)
-    console.log(guess, typeof guess);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-    if (!guess) {
-       document.querySelector(".message").textContent = "⛔ No Number "
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // when there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number ';
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '💪 Correct Number';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.message').style.color = 'red';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').style.color = 'blue';
+    
+
+    // when guess is too hight
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '✋ Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
     }
+
+    // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👎 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // when player lose the game
+      document.querySelector('.message').textContent = '💥You Lost The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
